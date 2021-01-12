37966380627de2ff742bd4faaef033c197c35f39
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-01-12 07:29:54 +0100

    Added error messages

--- sources/rune.c
+++ sources/rune.c
@@ -8,6 +8,10 @@ int main( int argc, char **argv[] ) {
   if ( argc > 2 ) {
     fprintf( "Usage:  rune < foo.rune >\n" );
   }
+
+  if (! ( src = fopen( argv[ 1 ], "r" ) ) ) {
+    return fprintf( stderr, "Unable to open file: '%s'\n", argv[ 1 ] );
+  }
   
   fopen( src );
   free ( /* tape or cell */ );
