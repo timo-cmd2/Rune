9d99ebed5b86f9a09bcd4eae72164c413ddd7589
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-01-12 07:23:44 +0100

    Update rune.c

--- sources/rune.c
+++ sources/rune.c
@@ -2,8 +2,14 @@
 #include <stdlib.h>
 
 int main( int argc, char **argv[] ) {
+  int c;
+  FILE *src;
+  
   if ( argc > 2 ) {
     fprintf( "Usage:  rune < foo.rune >\n" );
   }
+  
+  fopen( src );
+  free ( /* tape or cell */ );
   return 0;
 }
